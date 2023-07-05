1b58e094cf58c33cdbe7d758453d0ead01544908
u2piaaa <a59043622@163.com>
2023-07-05 16:16:31 +0800

    mdi图标组件

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
 const vuetify = createVuetify({
